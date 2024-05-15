eb1e42422558a0809fb659d49c6bf8ed2dc23715
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-05-15 12:47:00 +0300

    created wizard and added attack

--- Escape the Dark Dungeon/Assets/Scripts/Character/CharacterController.cs
+++ Escape the Dark Dungeon/Assets/Scripts/Character/CharacterController.cs
@@ -5,10 +5,14 @@ using UnityEngine.InputSystem;
 public class CharacterController : MonoBehaviour
 {
     private CharacterMovement characterMovement;
+    private SpriteRenderer spriteRenderer;
     private Vector2 pointerInput, movementInput;
     public Vector2 PointerInput => pointerInput;
     [SerializeField]
     private InputActionReference movementAction, pointerAction, attackAction;
+
+    public GameObject fireball;
+    public float fireballSpeed = 5f;
 
     private void onEnable()
     {
@@ -20,25 +24,30 @@ public class CharacterController : MonoBehaviour
         attackAction.action.performed -= CharacterAttack;
     }
 
-    private void CharacterAttack(InputAction.CallbackContext context)
-    {
-        throw new NotImplementedException();
-    }
 
     private void Awake()
     {
         characterMovement = GetComponent<CharacterMovement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void Update()
     {
         pointerInput = getMousePosition();
+        RotateCharacter();
         movementInput = movementAction.action.ReadValue<Vector2>();
         characterMovement.movementInput = movementInput;
 
         if(attackAction.action.triggered)
         {
-            Debug.Log("Attack");
+            CharacterAttack(default);
         }
+    }
+    private void CharacterAttack(InputAction.CallbackContext context)
+    {
+        GameObject fireball = Instantiate(this.fireball, transform.position, Quaternion.identity);
+        Rigidbody2D rb2 = fireball.GetComponent<Rigidbody2D>();
+        Vector2 direction = pointerInput - (Vector2)transform.position;
+        rb2.velocity = direction.normalized * fireballSpeed;
     }
 
     private Vector2 getMousePosition()
@@ -46,5 +55,16 @@ public class CharacterController : MonoBehaviour
         Vector3 position = pointerAction.action.ReadValue<Vector2>();
         position.z = Camera.main.nearClipPlane;
         return Camera.main.ScreenToWorldPoint(position);
+    }
+
+    private void RotateCharacter()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(
+        mousePosition.x - transform.position.x,
+        mousePosition.y - transform.position.y
+        );
+        transform.up = direction;
     }
 }
