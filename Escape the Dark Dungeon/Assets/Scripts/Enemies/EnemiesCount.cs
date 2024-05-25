46d1931b474574215538f2a10f69820c2c93652b
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-05-25 15:44:28 +0300

    fixed monster left to kill text

--- Escape the Dark Dungeon/Assets/Scripts/Enemies/EnemiesCount.cs
+++ Escape the Dark Dungeon/Assets/Scripts/Enemies/EnemiesCount.cs
@@ -9,20 +9,29 @@ public class EnemiesCount : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI countEnemies;
     private int killedEnemies = 0;
+    private int totalMonsters = 0;
+
     void Start()
     {
-        countEnemies.text = "0 / " + MonsterData.MonsterCounter + " Monsters Killed";
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.name.ToLower().Contains("monster"))
+            {
+                totalMonsters++;
+            }
+        }
+
+        countEnemies.text = killedEnemies + " / " + totalMonsters + " Monsters Killed";
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void UpdateKilledEnemies()
     {
         killedEnemies++;
-        countEnemies.text = killedEnemies + " / " + MonsterData.MonsterCounter + " Monsters Killed";
+        countEnemies.text = killedEnemies + " / " + totalMonsters + " Monsters Killed";
     }
 }
