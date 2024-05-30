7c9bb6806b74bb79e0ca0b5b206c7de94b3173ae
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-05-30 15:46:45 +0300

    added attack cds and cooldown on them

--- Escape the Dark Dungeon/Assets/Scripts/Character/ChangeAttack.cs
+++ Escape the Dark Dungeon/Assets/Scripts/Character/ChangeAttack.cs
@@ -11,27 +11,61 @@ public class ChangeAttack : MonoBehaviour
     private Image attackImage;
     [SerializeField]
     private TextMeshProUGUI attackText;
+    [SerializeField]
+    private Sprite fireballImage, iceballImage;
+    private float cooldownTime = 4.0f;
+    private float nextChangeTime = 0.0f;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        if(characterController == null)
+        if (characterController == null)
         {
             Debug.LogError("CharacterController not found");
         }
+        attackText.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        HandleAttackChange();
+        HandleAttackCooldown();
+    }
+
+    private void HandleAttackChange()
+    {
+        if (Time.time >= nextChangeTime)
         {
-            if(characterController.activeSpell == 1)
+            if (Input.GetKeyDown(KeyCode.Q))
             {
-                characterController.activeSpell = 2;
+                if (characterController.activeSpell == 1)
+                {
+                    characterController.activeSpell = 2;
+                    attackImage.sprite = fireballImage;
+                }
+                else
+                {
+                    characterController.activeSpell = 1;
+                    attackImage.sprite = iceballImage;
+                }
+                nextChangeTime = Time.time + cooldownTime;
             }
-            else
-            {
-                characterController.activeSpell = 1;
-            }
+        }
+    }
+
+    private void HandleAttackCooldown()
+    {
+        if (Time.time >= nextChangeTime)
+        {
+            attackText.gameObject.SetActive(false);
+            attackImage.color = new Color(1, 1, 1, 1);
+        }
+        else
+        {
+            float timeLeft = Mathf.Ceil(nextChangeTime - Time.time);
+            attackImage.color = new Color(1, 1, 1, 0.5f);
+            attackText.text = $"{timeLeft:F0}s";
+            attackText.gameObject.SetActive(true);
         }
     }
 }
--- Escape the Dark Dungeon/Assets/Scripts/Character/CharacterController.cs
+++ Escape the Dark Dungeon/Assets/Scripts/Character/CharacterController.cs
@@ -13,6 +13,12 @@ public class CharacterController : MonoBehaviour
     public GameObject fireballPrefab, iceballPrefab;
     public float fireballSpeed = 7f;
     public float iceballSpeed = 3.5f;
+    // Attacks cooldown 
+    public float fireballCooldown = 0.2f;
+    public float iceballCooldown = 0.8f;
+    // Timers set to 0 initially
+    private float fireballTimer = 0.0f;
+    private float iceballTimer = 0.0f;
 
     private void OnEnable()
     {
@@ -43,18 +49,28 @@ public class CharacterController : MonoBehaviour
         // Instantiate and launch the fireball
         if(activeSpell == 1)
         {
+            if(Time.time < fireballTimer)
+            {
+                return;
+            }
             GameObject fireball = Instantiate(fireballPrefab, transform.position, Quaternion.identity);
             Rigidbody2D rb2 = fireball.GetComponent<Rigidbody2D>();
             Vector2 direction = pointerInput - (Vector2)transform.position;
             rb2.velocity = direction.normalized * fireballSpeed;
+            fireballTimer = Time.time + fireballCooldown;
         }
         // Instantiate and launch the iceball
         else
         {
+            if(Time.time < iceballTimer)
+            {
+                return;
+            }
             GameObject iceball = Instantiate(iceballPrefab, transform.position, Quaternion.identity);
             Rigidbody2D rb2 = iceball.GetComponent<Rigidbody2D>();
             Vector2 direction = pointerInput - (Vector2)transform.position;
             rb2.velocity = direction.normalized * iceballSpeed;
+            iceballTimer = Time.time + iceballCooldown;
         }
     }
 
